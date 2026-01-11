e91d738c8f35bb0464344f6f3064f5fd285cda91
miyolppl-boop <miyolppl@gmail.com>
2026-01-11 09:22:52 +0300

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,14 @@
+const data = {
+  "solar-night": {
+    title: "Solar Night",
+    chapters: [
+      {
+        num: 1,
+        pages: [
+          "https://i.imgur.com/XXXX.jpg",
+          "https://i.imgur.com/YYYY.jpg"
+        ]
+      }
+    ]
+  }
+};
